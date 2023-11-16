be4a4b5156c5768cf2add6315868bdc3bd9e51f0
Luis <watchforname@gmail.com>
2023-11-16 16:41:39 -0300

    Alteracao de codigo. Build no projeto

--- src/app/services/lembrete.service.ts
+++ src/app/services/lembrete.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Lembrete } from '../interfaces/lembrete';
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class LembreteService {
 
-  lembreteUrl = 'http://localhost:3000/lembretes'
+  lembreteUrl = `${environment.baseUrl}`
 
   constructor(private http: HttpClient) { }
 
